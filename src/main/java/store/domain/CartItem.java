018f4b2a015bc464e690578ee18b102e2920ad26
yeonnhuu <yeonnhuu@gmail.com>
2024-11-11 23:12:25 +0900

    :sparkles: feat(CartItem): 구매할 상품 속성 제공 및 관리 기능 제공

--- src/main/java/store/domain/CartItem.java
+++ src/main/java/store/domain/CartItem.java
@@ -1,4 +1,45 @@
 package store.domain;
 
 public class CartItem {
+    private final Product product;
+    private final int totalQuantity;
+    private final int promotionSet;
+
+    public CartItem(Product product, int totalQuantity, int promotionSet) {
+        this.product = product;
+        this.totalQuantity = totalQuantity;
+        this.promotionSet = promotionSet;
+    }
+
+    public String getName() {
+        return product.getName();
+    }
+
+    public int totalPrice() {
+        return totalRetailPrice() + totalPromoPrice();
+    }
+
+    public int totalRetailPrice() {
+        return product.getPrice() * (totalQuantity - calcPromoQuantity());
+    }
+
+    public int totalPromoPrice() {
+        return product.getPrice() * calcPromoQuantity();
+    }
+
+    public int totalFreePrice() {
+        return product.getPrice() * calcFreeQuantity();
+    }
+
+    public int calcTotalQuantity() {
+        return totalQuantity;
+    }
+
+    private int calcPromoQuantity() {
+        return promotionSet * product.promoCycle();
+    }
+
+    public int calcFreeQuantity() {
+        return promotionSet * product.promoFree();
+    }
 }
