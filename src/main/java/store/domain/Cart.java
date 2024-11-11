a105d304eda5dfb269c2bcbafab8e505df0ed759
yeonnhuu <yeonnhuu@gmail.com>
2024-11-11 23:12:50 +0900

    :sparkles: feat(Cart): 구매할 상품 목록 속성 제공 및 관리 기능 제공

--- src/main/java/store/domain/Cart.java
+++ src/main/java/store/domain/Cart.java
@@ -1,4 +1,34 @@
 package store.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Cart {
+    private final List<CartItem> items = new ArrayList<>();
+
+    public void addItem(CartItem item) {
+        items.add(item);
+    }
+
+    public List<CartItem> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+
+    public int totalQuantity() {
+        return items.stream().mapToInt(CartItem::calcTotalQuantity).sum();
+    }
+
+    public int totalPrice() {
+        return items.stream().mapToInt(CartItem::totalPrice).sum();
+    }
+
+    public int totalRetailPrice() {
+        return items.stream().mapToInt(CartItem::totalRetailPrice).sum();
+    }
+
+    public int totalFreePrice() {
+        return items.stream().mapToInt(CartItem::totalFreePrice).sum();
+    }
 }
+
