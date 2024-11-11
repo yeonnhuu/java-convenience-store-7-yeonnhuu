afafac70d102b83cc949858be4c241f8ff1c3bb1
yeonnhuu <yeonnhuu@gmail.com>
2024-11-11 23:19:10 +0900

    :sparkles: feat(PaymentService): 결제 진행 관리 기능 제공

--- src/main/java/store/service/PaymentService.java
+++ src/main/java/store/service/PaymentService.java
@@ -1,4 +1,38 @@
 package store.service;
 
+import store.controller.CartManager;
+import store.domain.Cart;
+import store.domain.Receipt;
+
 public class PaymentService {
+    private static final double MEMBERSHIP_DISCOUNT_RATE = 0.3;
+    private final static int MEMBERSHIP_DISCOUNT_LIMIT = 8000;
+
+    private final CartManager cartManager;
+    private int membershipDiscountBalance;
+
+    public PaymentService(CartManager cartManager) {
+        this.cartManager = cartManager;
+        this.membershipDiscountBalance = MEMBERSHIP_DISCOUNT_LIMIT;
+    }
+
+    public Cart getCartItems(String purchaseList) {
+        return cartManager.generateCart(purchaseList);
+    }
+
+    public Receipt createReceipt(Cart cart, boolean applyMembership) {
+        int membershipDiscount = calcMembershipDiscount(applyMembership, cart.totalRetailPrice());
+        return new Receipt(cart, cart.totalPrice(), cart.totalFreePrice(), membershipDiscount);
+    }
+
+    private int calcMembershipDiscount(boolean apply, int eligibleAmount) {
+        if (apply) {
+            int discountAmount = Math.min((int) (eligibleAmount * MEMBERSHIP_DISCOUNT_RATE),
+                    membershipDiscountBalance);
+            membershipDiscountBalance -= discountAmount;
+
+            return discountAmount;
+        }
+        return 0;
+    }
 }
