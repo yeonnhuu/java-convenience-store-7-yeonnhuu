c6ec91a822107b2b02edfec641fbebacd9ce6a1d
yeonnhuu <yeonnhuu@gmail.com>
2024-11-11 23:10:13 +0900

    :sparkles: feat(Promotion): 상품 프로모션 속성 제공

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,4 +1,25 @@
 package store.domain;
 
 public class Promotion {
+    private final String name;
+    private final int buy;
+    private final int free;
+
+    public Promotion(String name, int buy, int free) {
+        this.name = name;
+        this.buy = buy;
+        this.free = free;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getFree() {
+        return free;
+    }
 }
